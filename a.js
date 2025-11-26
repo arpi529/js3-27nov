7bf5389cf37796c542c64fc931c61ed78b4c82c4
Arpit Himanshu <arpithimanshu277@gmail.com>
2025-11-26 22:56:18 +0530

    Add script to calculate sum of an array

---
+++ a.js
@@ -0,0 +1,8 @@
+let numbers = [10, 20, 30, 40, 50];  // Example array with 5 numerical values
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log("The sum of array values is: " + sum);
